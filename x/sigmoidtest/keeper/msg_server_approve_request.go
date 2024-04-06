2d54b828928d2af66fbb5b5ada82b7763b5dfcd2
NIkolay <nikhovas@ip-172-31-28-192.eu-central-1.compute.internal>
2024-04-06 13:22:28 +0000

    logic for adding transactions to front list

--- x/sigmoidtest/keeper/msg_server_approve_request.go
+++ x/sigmoidtest/keeper/msg_server_approve_request.go
@@ -6,6 +6,8 @@ import (
 	"sigmoid-test/x/sigmoidtest/types"
 
 	"cosmossdk.io/math"
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -33,11 +35,17 @@ func (k msgServer) ApproveRequest(goCtx context.Context, msg *types.MsgApproveRe
 	k.Keeper.SetLastProcessedTransaction(ctx, msg.TransactionId)
 	k.Keeper.RemoveRequest(ctx, &msg.SenderAddress)
 
-	// storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	// mintStore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.RequestsByMintAddressKey(request.MintAddress)))
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	mintStore := prefix.NewStore(storeAdapter, types.KeyPrefix(types.RequestsByMintAddressKey(request.MintAddress)))
 
-	// appendedValue := k.cdc.MustMarshal(&request)
-	// mintStore.Set([]byte(request.SenderAddress), appendedValue)
+	appendedValue := k.cdc.MustMarshal(&types.FrontStakeRequest{
+		Sender:        request.SenderAddress,
+		Mint:          request.MintAddress,
+		Status:        request.Status,
+		Amount:        request.Amount,
+		TransactionId: msg.TransactionId,
+	})
+	mintStore.Set([]byte(msg.TransactionId), appendedValue)
 
 	return &types.MsgApproveRequestResponse{}, nil
 }
