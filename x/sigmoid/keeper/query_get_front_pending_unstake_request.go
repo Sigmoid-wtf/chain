67ca8f926cb6702d0619b507ba1ee5e54007e6c9
nikhovas <nikhovas@yandex.ru>
2024-04-29 01:44:54 +0300

    ad logic for front unstake request

--- x/sigmoid/keeper/query_get_front_pending_unstake_request.go
+++ x/sigmoid/keeper/query_get_front_pending_unstake_request.go
@@ -5,6 +5,8 @@ import (
 
 	"sigmoid/x/sigmoid/types"
 
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -16,9 +18,27 @@ func (k Keeper) GetFrontPendingUnstakeRequest(goCtx context.Context, req *types.
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.UnstakeRequestsKey))
+	iterator := store.Iterator(nil, nil)
+	defer iterator.Close()
 
-	// TODO: Process the query
-	_ = ctx
+	var unstakeRequest types.MsgCreateUnstakeRequest
+	for ; iterator.Valid(); iterator.Next() {
+		if iterator.Error() != nil {
+			return nil, iterator.Error()
+		}
 
-	return &types.QueryGetFrontPendingUnstakeRequestResponse{}, nil
+		k.cdc.MustUnmarshal(iterator.Value(), &unstakeRequest)
+		if unstakeRequest.Creator == req.Address {
+			break
+		}
+	}
+
+	return &types.QueryGetFrontPendingUnstakeRequestResponse{Request: &types.Request{
+		SenderAddress: unstakeRequest.Creator,
+		MintAddress:   unstakeRequest.UnstakeAddress,
+		Amount:        unstakeRequest.Amount,
+		Status:        0,
+	}}, nil
 }
