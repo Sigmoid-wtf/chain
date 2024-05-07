cf0789eecbd5dfb5a7796f1b9d231b32ab1ecfc1
Alexander Nikolaev <alexander.nikolaev.v@gmail.com>
2024-05-07 21:29:09 +0000

    approve-bridge-request logic

--- x/sigmoid/keeper/msg_server_approve_bridge_request.go
+++ x/sigmoid/keeper/msg_server_approve_bridge_request.go
@@ -6,13 +6,18 @@ import (
 	"sigmoid/x/sigmoid/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) ApproveBridgeRequest(goCtx context.Context, msg *types.MsgApproveBridgeRequest) (*types.MsgApproveBridgeRequestResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	_, found := k.GetBridgeRequest(ctx, &msg.Address)
+	if !found {
+		return nil, sdkerrors.ErrInvalidAddress
+	}
+
+	k.RemoveBridgeRequest(ctx, &msg.Address)
 
 	return &types.MsgApproveBridgeRequestResponse{}, nil
 }
