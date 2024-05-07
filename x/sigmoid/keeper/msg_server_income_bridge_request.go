4d4d8cd22a028fe43e099a1d37892f0df3581d1a
Alexander Nikolaev <alexander.nikolaev.v@gmail.com>
2024-05-07 21:49:37 +0000

    income-bridge-request logic

--- x/sigmoid/keeper/msg_server_income_bridge_request.go
+++ x/sigmoid/keeper/msg_server_income_bridge_request.go
@@ -5,14 +5,23 @@ import (
 
 	"sigmoid/x/sigmoid/types"
 
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) IncomeBridgeRequest(goCtx context.Context, msg *types.MsgIncomeBridgeRequest) (*types.MsgIncomeBridgeRequestResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	from := sdk.MustAccAddressFromBech32(types.BridgeTreasuryAddress)
+	to := sdk.MustAccAddressFromBech32(msg.Address)
+
+	coins := sdk.NewCoins(sdk.NewCoin("sigRAO", math.NewIntFromUint64(msg.Amount)))
+
+	err := k.Keeper.bankKeeper.SendCoins(ctx, from, to, coins)
+	if err != nil {
+		return nil, sdkerrors.ErrInvalidCoins
+	}
 
 	return &types.MsgIncomeBridgeRequestResponse{}, nil
 }
