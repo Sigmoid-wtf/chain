2699dc42c7a915dcc094292b522b3bd8701d1d28
Alexander Nikolaev <alexander.nikolaev.v@gmail.com>
2024-05-07 20:44:24 +0000

    get-pending-bridge-request logic

--- x/sigmoid/keeper/query_get_pending_bridge_request.go
+++ x/sigmoid/keeper/query_get_pending_bridge_request.go
@@ -3,21 +3,35 @@ package keeper
 import (
 	"context"
 
-    "sigmoid/x/sigmoid/types"
+	"sigmoid/x/sigmoid/types"
+
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) GetPendingBridgeRequest(goCtx context.Context,  req *types.QueryGetPendingBridgeRequestRequest) (*types.QueryGetPendingBridgeRequestResponse, error) {
+func (k Keeper) GetPendingBridgeRequest(goCtx context.Context, req *types.QueryGetPendingBridgeRequestRequest) (*types.QueryGetPendingBridgeRequestResponse, error) {
 	if req == nil {
-        return nil, status.Error(codes.InvalidArgument, "invalid request")
-    }
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.PendingBridgeRequestsKey))
+	iterator := store.Iterator(nil, nil)
+	defer iterator.Close()
 
-    // TODO: Process the query
-    _ = ctx
+	var bridgeRequest types.MsgCreateBridgeRequest
+	for ; iterator.Valid(); iterator.Next() {
+		if iterator.Error() != nil {
+			return nil, iterator.Error()
+		}
 
-	return &types.QueryGetPendingBridgeRequestResponse{}, nil
+		k.cdc.MustUnmarshal(iterator.Value(), &bridgeRequest)
+		break
+	}
+
+	return &types.QueryGetPendingBridgeRequestResponse{Request: &bridgeRequest}, nil
 }
