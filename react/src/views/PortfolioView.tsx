5f9b80e5ed6c474ce9e323388743694d065e070d
nikhovas <nikhovas@yandex.ru>
2024-06-02 15:35:53 +0300

    add wallet integration and rate

--- react/src/views/PortfolioView.tsx
+++ react/src/views/PortfolioView.tsx
@@ -4,13 +4,28 @@ import IgntAssets from "../components/IgntAssets";
 import IgntStakeUnstakes from "../components/IgntStakeUnstakes";
 import IgntTransactions from "../components/IgntTransactions";
 import IgntTransfer from "../components/IgntTransfer";
+import useSigmoidSigmoid from "../hooks/useSigmoidSigmoid";
 
 export default function PortfolioView() {
+  const { QueryGetSigtaoRateD } = useSigmoidSigmoid();
+  const rateRsp = QueryGetSigtaoRateD({});
+
   return (
     <div>
       <div className="container mx-auto">
         <div className="grid grid-cols-2">
           <div>
+            <header className="flex items-center justify-between">
+              <h2 className="text-3xl text-black font-semibold p-0 m-0 mb-2.5 flex-1" style={{ marginLeft: 10 }}>
+                Current SigTAO rate
+              </h2>
+            </header>
+            <div
+              className="text-left text-black opacity-75 text-md font-normal"
+              style={{ marginLeft: 10, marginBottom: 20 }}
+            >
+              Current Rate: {rateRsp.data?.sigtaoRateD}
+            </div>
             <IgntAssets className="px-2.5 mb-10" displayLimit={3} />
             <IgntTransactions className="px-2.5" />
           </div>
